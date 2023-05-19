52256d7f03120231eab65d90e07939369dab71ab
gitlab <nikisazikov56@gmail.com>
2023-05-19 03:56:57 +0300

    First working version

--- tools/server.sh
+++ tools/server.sh
@@ -2,13 +2,5 @@
 
 #make ../
 g++  ../src/main.cpp ../src/*/*.cpp -lboost_thread -lboost_timer -lboost_chrono -lrt
-echo $?
-if [[ $? -e 0 ]]; then
-    echo "good"
-    sudo ./a.out
-else 
-    echo "success"
-fi;
-
 
 
